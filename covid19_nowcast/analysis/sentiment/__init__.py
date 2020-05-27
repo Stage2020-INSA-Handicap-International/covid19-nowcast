b7ef0a765c65c9e374d28968a4c853052cceab71
mbettinger <matthieu.bettinger@insa-lyon.fr@users.noreply.github.com>
2020-05-27 11:07:13 +0200

    basic workflow Naive Bayes

--- covid19_nowcast/analysis/sentiment/__init__.py
+++ covid19_nowcast/analysis/sentiment/__init__.py
@@ -1,5 +1,9 @@
 from nltk.classify import NaiveBayesClassifier
 
-def train_classifier(bag_of_words, **kwargs):
-    classifier = NaiveBayesClassifier.train(bag_of_words)
+def train_classifier(training_set, **kwargs):
+    classifier = NaiveBayesClassifier.train(training_set)
     return {"sentiment_classifier":classifier}
+
+def classify(sentiment_classifier, tweets, **kwargs):
+    classified_tweets = [{tweet:sentiment_classifier.classify(tweet)} for tweet in tweets]
+    return {"sentiment_labelled_tweets":classified_tweets}
