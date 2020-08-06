0065b09cfbd5aea21a84b2046dfe638d6017b077
mbettinger <matthieu.bettinger@insa-lyon.fr@users.noreply.github.com>
2020-08-06 15:55:52 +0200

    Classifiers Imports

--- covid19_nowcast/streaming/collection/__init__.py
+++ covid19_nowcast/streaming/collection/__init__.py
@@ -7,7 +7,11 @@ import covid19_nowcast.streaming.collection.articles
 
 from covid19_nowcast.streaming.collection.config_auth_twitter import *
 import twitter
-
+from covid19_nowcast import util
 def authenticate(consumer_key= consumer_key, consumer_secret=consumer_secret, access_token_key=access_token_key, access_token_secret=access_token_secret, **kwargs):
     api = twitter.Api(consumer_key,consumer_secret,access_token_key,access_token_secret)
     return api
+
+def collect_sts_data(country,source,date_from,date_to):
+    data=util.import_params("../output/topics_india_tw0.json")["tweets"]
+    return data
