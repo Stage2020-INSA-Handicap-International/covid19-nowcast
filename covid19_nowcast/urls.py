ea538818baba18f7af61bfe8e8db2b62db67161c
SalimHF <salim.hafid@insa-lyon.fr>
2020-08-09 00:34:45 +0200

    Django server running basic Vue Single-Page-App

--- covid19_nowcast/urls.py
+++ covid19_nowcast/urls.py
@@ -1,8 +1,17 @@
 from django.urls import include, path
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
 
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",
+        TemplateView.as_view(template_name="application.html"),
+        name="app",
+    ),
     path(r'',views.index),
     path(r'collector/',views.CollectorView.as_view()),
     path(r'topics/',views.TopicAnalysisView.as_view()),
