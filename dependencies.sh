65a53b630b4ac5280735cdf9de7949fbc4ecbda7
Gilles Vester <49953678+mbettinger@users.noreply.github.com>
2020-08-17 18:42:03 +0200

    Dependencies

---
+++ dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt-get install graphviz mongodb npm
+python3 -m pip install -r requirements
+npm install node-ipc
+npm install --save-dev webpack-bundle-tracker@0.4.3
+npm install --save chart.js jquery moment
